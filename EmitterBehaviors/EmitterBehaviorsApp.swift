47dc68f3b443143f811d4f4e7b70a668471f070a
Erik Olsson <valross2@gmail.com>
2021-03-21 20:27:32 +0100

    Initial Commit

---
+++ EmitterBehaviors/EmitterBehaviorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmitterBehaviorsApp.swift
+//  EmitterBehaviors
+//
+//  Created by Erik Olsson on 2021-03-21.
+//
+
+import SwiftUI
+
+@main
+struct EmitterBehaviorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
